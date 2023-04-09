6aa7191891658821e329d9eb46731b373b92a7ae
Vladislav T <satari692000@yahoo.com>
2023-04-09 17:29:55 +0300

    Added sending to recepients

--- src/main/java/com/primforest/tgbot2/Service/TelegramBot.java
+++ src/main/java/com/primforest/tgbot2/Service/TelegramBot.java
@@ -14,14 +14,15 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -48,6 +49,18 @@ public class TelegramBot extends TelegramLongPollingBot{
         if(update.hasMessage()&update.getMessage().hasText()){
             String messageText=update.getMessage().getText();
             long chatId=update.getMessage().getChatId();
+
+            if (messageText.contains("/send")&&config.getOwnerId()==chatId){
+                var textToSend=EmojiParser.parseToUnicode(messageText.substring(messageText.indexOf("")));
+                var users=userRepository.findAll();
+                for (User user:users){
+                    try {
+                        sendMessage(user.getChatId(),textToSend);
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
             switch (messageText) {
                 case "/start":
 
@@ -59,6 +72,7 @@ public class TelegramBot extends TelegramLongPollingBot{
                     }
 
                     break;
+
                 case"/help":
                     try {
                         sendMessage(chatId,HELP_TEXT);
@@ -66,6 +80,10 @@ public class TelegramBot extends TelegramLongPollingBot{
                         throw new RuntimeException(e);
                     }
                     break;
+                case  "/register":
+                    register(chatId);
+                    break;
+
                 default:{
 
                     try {
@@ -78,6 +96,31 @@ public class TelegramBot extends TelegramLongPollingBot{
                 }
         }
     }
+
+    private void register(long chatId) {
+       SendMessage message=new SendMessage();
+       message.setChatId(String.valueOf(chatId));
+       message.setText("Do your really want to register?");
+        InlineKeyboardMarkup markupInline= new InlineKeyboardMarkup() ;
+        List<List<InlineKeyboardButton>> rowsInLine=new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine=new ArrayList<>();
+        var yesButton =new InlineKeyboardButton();
+        yesButton.setText("Yes");
+        yesButton.setCallbackData("YES_BUTTON");
+        var noButton=new InlineKeyboardButton();
+        noButton.setText("No");
+        noButton.setCallbackData("NO_BUTTON");
+        rowInLine.add(yesButton);
+        rowInLine.add(noButton);
+        rowsInLine.add(rowInLine);
+        markupInline.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markupInline);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+   }
 
     @Override
     public void onUpdatesReceived(List<Update> updates) {
@@ -93,6 +136,19 @@ public class TelegramBot extends TelegramLongPollingBot{
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
+        ReplyKeyboardMarkup keyboardMarkup=new ReplyKeyboardMarkup();
+        List<KeyboardRow>keyboardRows=new ArrayList<>();
+        KeyboardRow row= new KeyboardRow();
+        row.add("Weather");
+        row.add("get random joke");
+        keyboardRows.add(row);
+        row.add("register");
+        row.add(" check my mydata");
+        row.add("delete my data");
+        keyboardRows.add(row);
+        keyboardMarkup.setKeyboard(keyboardRows);
+        message.setReplyMarkup(keyboardMarkup );
+
         try {
             execute(message);
         } catch (TelegramApiException e) {
--- src/main/java/com/primforest/tgbot2/config/BotConfig.java
+++ src/main/java/com/primforest/tgbot2/config/BotConfig.java
@@ -16,4 +16,6 @@ public class BotConfig {
     String botName;
     @Value("${bot.token}")
     String token;
+  @Value("${bot.owner}")
+    Long ownerId;
 }
